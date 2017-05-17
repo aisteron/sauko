fac206c8d63cfb550910c548fcb671bb4234141b
aisteron <aisteron@yandex.ru>
2017-05-17 22:46:21 +0300

    3 section done

---
+++ Gruntfile.js
@@ -0,0 +1,107 @@
+module.exports = function(grunt){
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+
+
+		pug: 
+			{
+			  compile: {
+			    options: {
+			      data: {
+			        debug: false
+			      }
+			    },
+			    files: {
+			      'index.html': ['dev/index.pug']
+
+			    }
+			  }
+			},
+
+		less: 
+			{
+
+			  production: {
+			    options: 
+				    {
+				      paths: ['dev/css'],
+				      plugins: 
+				      	[
+				        new (require('less-plugin-autoprefix'))({browsers: ["last 3 versions"]})
+				        //,new (require('less-plugin-clean-css'))()
+				        ],
+				       sourceMap:true, 
+				       sourceMapFilename: 'dev/css/custom.css.map', // where file is generated and located
+			           sourceMapURL: 'dev/css/custom.css.map', // the complete url and filename put in the compiled css file
+			           sourceMapBasepath: 'dev', // Sets sourcemap base path, defaults to current working directory.
+			           sourceMapRootpath: '/', // adds this path onto the sourcemap filename and less file paths
+        			
+				    },
+			    files: 
+				    {
+				      'dev/css/custom.css': 'dev/css/custom.less'
+				    }
+			  }
+			},
+
+		concat: 
+			{
+				dist:
+					{
+						src:['dev/css/custom.css'],
+						dest:'dev/css/concat.css'
+					}
+  			},	
+
+		watch:
+			{
+				css:
+				{
+					files:['dev/css/custom.less', 'dev/css/colors.less', 'dev/css/media-queries.less'],
+					tasks:['less', 'concat'],
+					options: {
+				      livereload: true,
+				    }
+				},
+				scripts:
+				{
+					files:['dev/index.pug'],
+					tasks:['pug'],
+					options: {
+				      livereload: true,
+				    }
+					
+				}
+			},
+
+		browserSync: 
+			{
+	            dev: {
+	                bsFiles: {
+	                    src : [
+                        'dev/css/*.css',
+                        '*.html'
+                    ]
+	                },
+	                options: {
+	                    watchTask: true,
+	                    proxy: 'sauko.local'
+	                }
+	            }
+	        }	
+
+	}); //end .initConfig
+
+	grunt.loadNpmTasks('grunt-contrib-pug');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-browser-sync');
+
+	grunt.registerTask('default',['pug','less','concat','browserSync','watch'])
+
+
+}; //end wrap

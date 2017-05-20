f99b848d910526cabbf94087de5802c0277e526a
aisteron <timotheus@list.ru>
2017-05-20 09:22:27 +0300

    настроил отправку писем с лендинга

--- send.php
+++ send.php
@@ -1,35 +1,47 @@
-<?php
-//echo $_POST['name'].$_POST['phone'];
+<?php<?php
 
-	$temail =  $_POST['temail'];
-	$name = $_POST['name'];			
-	$phone = $_POST['phone'];
-
-	if($temail)
-	{
-		$to = "timotheus@list.ru";
-
-		$subject = "Новый подписчик с лендинга sauko.by";
-		$txt = "Вот его почта: ". $temail;
-		$headers = "From: robot@sauko.by";
-
-		mail($to,$subject,$txt,$headers);
-
+		if($_POST['temail'])
+		{	
+		$to = 'timotheus@list.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Новый подписчик с лендинга sauko.by'; //Заголовок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+					<h3>Вот его почта</h3>
+                        <p><b>Почта: </b>'.$_POST['temail'].'</p>
+						<p><small>Не забудь включить его в список для рассылки ^_^</small></p>
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: robot <robot@sauko.by>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
 		echo 'Спасибо за доверие! Ждите писем!';
-	}
-		elseif($name and $phone)
-			{
-				$to = "timotheus@list.ru";
+		}
+		elseif($_POST['name'] and $_POST['phone'])
+		{
+		$to = 'timotheus@list.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Перезвонить заказчику | sauko.by'; //Заголовок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+					<h3>Форма обратной связи на сайте sauko.by:</h3>
+                        <p><b>Имя: </b>'.$_POST['name'].'</p>
+						<p><b>Телефон: </b>'.$_POST['phone'].'</p>                      
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: robot <robot@sauko.by>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+		echo 'Ура! Мы получили уведомление и скоро вам перезвоним!';
 
-				$subject = "Перезвонить заказчику | sauko.by";
-				$txt = "Имярек: ". $name.'<br>';
-				$txt .= "Телефон: ". $phone;
-				$headers = "From: robot@sauko.by";
-
-				mail($to,$subject,$txt,$headers);
-
-				echo 'Ура! Мы получили уведомление и скоро вам перезвоним!';
-			} else
+		}
+		 else
 			{
 				echo 'Что-то пошло не так.. попробуйте позже?';
 			}
